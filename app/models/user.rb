4d6a4d7291e00414cfc3858cc234edc5ed6913b2
Brian Lee <email": null>
2016-07-08 11:49:55 -0400

    Add password minimum length validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ActiveRecord::Base
+  before_save { self.email = email.downcase }
   validates :username, :email, :password, presence: true
   validates :username, :email, uniqueness: true
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email,  format: { with: VALID_EMAIL_REGEX }
+  validates :password, length: { minimum: 6 }
 
 
   has_secure_password
